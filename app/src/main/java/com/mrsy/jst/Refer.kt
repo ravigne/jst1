c65dc0a872ac198eb0cc46da099aeba0a88d0737
ravi <ravignelyf@gmail.com>
2021-04-14 17:49:54 +0530

    hello

---
+++ app/src/main/java/com/mrsy/jst/Refer.kt
@@ -0,0 +1,11 @@
+package com.mrsy.jst
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Refer : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_refer)
+    }
+}
